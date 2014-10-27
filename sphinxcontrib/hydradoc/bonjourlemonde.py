bcf364448421011ad93ff6de455ee115ab8c5a4f
chbrun <christophe.brun.cl194@gadz.org>
2014-10-27 16:56:14 +0100

    Add links on class

--- sphinxcontrib/hydradoc/bonjourlemonde.py
+++ sphinxcontrib/hydradoc/bonjourlemonde.py
@@ -18,6 +18,7 @@ class bonjourlemondeDirective(Directive):
         env = self.state.document.settings.env
 
         jdl_data = {}
+        sphinx_nodes =[]
         # TODO find 'source' path to have the right path to the file
         hydra_filepath = '{0}/{1}'.format(os.getcwd(), self.content[0])
         with open(hydra_filepath) as f:
@@ -27,14 +28,24 @@ class bonjourlemondeDirective(Directive):
             for sc in jld_data['supportedClass']:
                 supportedClass.append(sc['@id'])
 
+                node = nodes.paragraph('', '')
+                data = 'class : {0}'.format(supportedClass)
+                newnode = nodes.reference('', '')
+                innernode = nodes.emphasis(data, data)
+                newnode['refdocname'] = 'docname'
+                newnode['refuri'] = supportedClass[0]
+                newnode.append(innernode)
+                node += newnode
+                sphinx_nodes.append(node)
+
+
             data = u'Liste des classes declarees {0} : \
                      avec les options {1}'.format(supportedClass,
                                                   self.content)
 
         #data = 'env : {0}'.format(dir(env))
 
-        node = nodes.Text(data, data)
-        return [node]
+        return sphinx_nodes
 
 
 def setup(app):
